ea7912aacf615dc987db62b48f19fa7ddea5b61a
aashr-n <aashray.mn@gmail.com>
2026-03-03 09:42:04 -0800

    swift proj

---
+++ CortiQuizSwift/CortiQuizSwift/CortiQuizSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortiQuizSwiftApp.swift
+//  CortiQuizSwift
+//
+//  Created by Aashray N on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CortiQuizSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CortiQuizSwift/CortiQuizSwiftTests/CortiQuizSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  CortiQuizSwiftTests.swift
+//  CortiQuizSwiftTests
+//
+//  Created by Aashray N on 3/2/26.
+//
+
+import Testing
+@testable import CortiQuizSwift
+
+struct CortiQuizSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
